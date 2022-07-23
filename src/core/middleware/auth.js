ef4bf498ff6afb4b6e4cfde3d36dcc2c5203da06
Shaghayeghnaimi <shaghayegh.naimi96@gmail.com>
2022-07-23 17:52:27 +0430

    paylod is created

--- src/core/middleware/auth.js
+++ src/core/middleware/auth.js
@@ -1,8 +1,27 @@
-class AuthMiddleWare{
-    static async login (req, res, next){
-        try{
-            
-        }
+const UserReader = require('../../module/users/model/read');
+const AuthenticationManager = require('../auth');
 
-    }
+class AuthMiddleWare{
+  
+    static async login(req, res, next) {
+        try {
+          const {email, password} = req.body;
+          const user = await UserReader.getUsersByEmailAndPassword(email, password);
+          if (!user) {
+            res.status(401).end();
+          } else {
+            const payload = {
+              id: user.id,
+              email: user.email,
+            };
+            const jwt = AuthenticationManager.getJwtToken(payload);
+            res.cookie('token', jwt.token, {
+              httpOnly: true,
+              maxAge: jwt.expirySecond * 1000
+            }).end();
+          }
+        } catch (error) {
+          res.status(500).send(error.message);
+        }
+      }
 }
