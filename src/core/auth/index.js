314277e59135d6cf55f96b254ce6719d25e68dbc
Shaghayeghnaimi <shaghayegh.naimi96@gmail.com>
2022-07-23 23:54:46 +0430

    token is build

--- src/core/auth/index.js
+++ src/core/auth/index.js
@@ -1,3 +1,22 @@
-class AuthenticationManger{
-    
+const jwt = require('jsonwebtoken');
+const {jwt: jwt_config} = require('../../config');
+
+class AuthenticationManager {
+  static getJwtToken(payload) {
+    const expirySecond = Number(jwt_config.expirySecond);
+    const token = jwt.sign(payload, jwt_config.secret, {
+      algorithm: 'HS256',
+      expiresIn: expirySecond
+    });
+    return {
+      token,
+      expirySecond
+    };
+  }
+  
+  static getJwtTokenPayload(token) {
+    return jwt.verify(token, jwt_config.secret);
+  }
 }
+
+module.exports = AuthenticationManager;
