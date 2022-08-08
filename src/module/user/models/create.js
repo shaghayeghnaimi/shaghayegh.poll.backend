68429a85471a67638296d728957c4c3611436174
Shaghayeghnaimi <shaghayegh.naimi96@gmail.com>
2022-08-08 18:54:33 +0430

    Poll is created

--- src/module/user/models/create.js
+++ src/module/user/models/create.js
@@ -1,15 +1,17 @@
 const DatabaseManager = require('../../../core/database/databaseManager');
+const saltRounds = 10;
+const bcrypt = require("bcrypt");
 
 
 class UserCreator {
-  
-  static createUser(userData) {
+  static async createUser(userData) {
     const {user_name, email, pass, first_name, last_name} = userData;
+    const hashedPass = await bcrypt.hash(pass, saltRounds)
     const query = `
       insert into user
       (user_name, email, pass, first_name, last_name)
       values
-      ('${user_name}', '${email}', '${pass}', '${first_name}'. '${last_name}' );
+      ('${user_name}', '${email}', '${hashedPass}', '${first_name}', '${last_name}' );
     `;
     return DatabaseManager.query(query);
   }
