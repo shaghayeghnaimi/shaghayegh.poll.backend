704c208289d1c286fff4fa9c3f8d4478bb16694b
Shaghayeghnaimi <shaghayegh.naimi96@gmail.com>
2022-08-11 14:49:57 +0430

    route for reading all polls is completed

--- src/module/poll/route.js
+++ src/module/poll/route.js
@@ -1,13 +1,12 @@
 const express = require('express');
-const AddressController = require('./controller');
+const PollController = require('./controller');
 
 const router = express.Router();
 
 
-router.get('/', AddressController.getAlladresses);
-router.get('/id/:id', AddressController.getaddressById);
-router.post('/', AddressController.createAddress);
-router.patch('/', AddressController.updateAddress )
-router.delete('/', AddressController.deleteAddress)
+router.get('/', PollController.getAllPolls);
+router.post('/', PollController.createPoll);
+// router.patch('/', AddressController.updateAddress )
+// router.delete('/', AddressController.deleteAddress)
 
 module.exports = router;
