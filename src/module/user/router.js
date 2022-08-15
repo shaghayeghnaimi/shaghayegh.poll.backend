d9da21caacf6863d46579c3f11166f82e2c7864d
Shaghayeghnaimi <shaghayegh.naimi96@gmail.com>
2022-08-15 21:08:49 +0430

    add celebrate and joi function property

--- src/module/user/router.js
+++ src/module/user/router.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const UserController = require('./controller');
 const AuthMiddleware = require('../../core/middleware/auth');
+const UserValidator = require('./validation');
 
 const router = express.Router();
 
 
 router.get('/', AuthMiddleware.jwtTokenValidation, UserController.getAllUsers);
 router.get('/id/:id', AuthMiddleware.jwtTokenValidation, UserController.getUserById);
-router.post('/', UserController.createUser);
+router.post('/', UserValidator.createUserSchema, UserController.createUser);
+
 
 module.exports = router;
